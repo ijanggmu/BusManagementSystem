04a57c7728912b7954904d57747111e391dacb9c
corion <corion.pradhan@nestrait.com>
2022-04-12 15:07:30 +0545

    Route services added

---
+++ Bus.Services/IRouteService.cs
@@ -0,0 +1,17 @@
+﻿using Bus.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bus.Services
+{
+    public interface IRouteService
+    {
+        IEnumerable<Route> GetAllRoute();
+        Route GetRouteById(int id);
+
+        void InserRoute(Route route);
+        void UpdaetRoute(Route route);
+        void DeleteRoute(int id);
+    }
+}
