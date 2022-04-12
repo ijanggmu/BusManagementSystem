7d430db25069bb5044a970089152fefb6b03ed6d
corion <corion.pradhan@nestrait.com>
2022-04-12 13:25:10 +0545

    Cloned

---
+++ Bus.Data/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bus.Data
+{
+    public class Route
+    {
+        [Key]
+        public int RouteId { get; set; }
+
+        public int NumberOfStops { get; set; }
+
+        public int BusCount { get; set; }
+
+    }
+}
