4017798853d1e6fd250472023d28575e5ebadf6d
corion <corion.pradhan@nestrait.com>
2022-04-12 14:48:50 +0545

    IRepository created

---
+++ Bus.Repo/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bus.Data;
+
+namespace Bus.Repo
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _db;
+        private DbSet<T> entities;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            entities = _db.Set<T>();
+        }
+        public void Create(T entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            _db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Remove(entity);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            _db.SaveChanges();
+        }
+    }
+}
