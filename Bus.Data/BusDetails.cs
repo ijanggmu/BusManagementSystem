d4257f95c7ff3c7330636c315da1ecf7585d0a60
corion <corion.pradhan@nestrait.com>
2022-04-12 14:12:08 +0545

    Busdetails added

---
+++ Bus.Data/BusDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bus.Data
+{
+    public class BusDetails
+    {
+        public int BusId { get; set; }
+
+        public string BusName { get; set; }
+    }
+}
