34673640a25f8ccc8b6e8d2fe13ef307cfa456d8
ijanggmu <sthaejan00@gmail.com>
2022-12-18 19:26:49 +0545

    :arrow_up:(upgrade:project)Upgrade project from 3.1 to 6.0

--- Bus.Web/Program.cs
+++ Bus.Web/Program.cs
@@ -1,20 +1,89 @@
+using Bus.Repo;
+using Bus.Services;
+using Bus.Services.Contracts;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Bus.Web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+//namespace Bus.Web
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder.UseStartup<Startup>();
+//                });
+//    }
+//}
+var builder = WebApplication.CreateBuilder(args);
+
+// Add builder.Services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                   builder.Configuration.GetConnectionString("DefaultConnection"))
+                );
+builder.Services.ConfigureApplicationServices();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddTransient<IRouteService, RouteService>();
+builder.Services.AddTransient<IBusdetailsService, BusDetailsService>();
+
+
+////services.AddAuthentication("CookieAuth")
+////    .AddCookie("CookieAuth", config =>
+////    {
+////        config.Cookie.Name = "Gradmas.Cookie";
+////        config.LoginPath = "/BusDetails/Authenticate";
+////    });
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddControllers();
+
+//builder.Services.AddDbContext<ProductContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+
+//});
+
+//builder.Services.AddScoped<IProductService, ProductService>();
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+//who are you?
+app.UseAuthentication();
+//are you allowed?
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
--- Bus.Web/Startup.cs
+++ Bus.Web/Startup.cs
@@ -1,80 +1,80 @@
-using Bus.Repo;
-using Bus.Services;
-using Bus.Services.Contracts;
-using MediatR;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Reflection;
+//using Bus.Repo;
+//using Bus.Services;
+//using Bus.Services.Contracts;
+//using MediatR;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using System.Reflection;
 
-namespace Bus.Web
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+//namespace Bus.Web
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
 
-        public IConfiguration Configuration { get; }
+//        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection"))
-                );
-            services.ConfigureApplicationServices();
-            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            services.AddTransient<IRouteService, RouteService>();
-            services.AddTransient<IBusdetailsService, BusDetailsService>();
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddDbContext<ApplicationDbContext>(
+//                options => options.UseSqlServer(
+//                    Configuration.GetConnectionString("DefaultConnection"))
+//                );
+//            services.ConfigureApplicationServices();
+//            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+//            services.AddTransient<IRouteService, RouteService>();
+//            services.AddTransient<IBusdetailsService, BusDetailsService>();
 
 
-            ////services.AddAuthentication("CookieAuth")
-            ////    .AddCookie("CookieAuth", config =>
-            ////    {
-            ////        config.Cookie.Name = "Gradmas.Cookie";
-            ////        config.LoginPath = "/BusDetails/Authenticate";
-            ////    });
-            services.AddControllersWithViews();
+//            ////services.AddAuthentication("CookieAuth")
+//            ////    .AddCookie("CookieAuth", config =>
+//            ////    {
+//            ////        config.Cookie.Name = "Gradmas.Cookie";
+//            ////        config.LoginPath = "/BusDetails/Authenticate";
+//            ////    });
+//            services.AddControllersWithViews();
 
-        }
+//        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                app.UseHsts();
+//            }
+//            app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
+//            app.UseStaticFiles();
 
-            app.UseRouting();
+//            app.UseRouting();
 
-            //who are you?
-            app.UseAuthentication();
+//            //who are you?
+//            app.UseAuthentication();
 
-            //are you allowed?
-            app.UseAuthorization();
+//            //are you allowed?
+//            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
-}
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllerRoute(
+//                    name: "default",
+//                    pattern: "{controller=Home}/{action=Index}/{id?}");
+//            });
+//        }
+//    }
+//}
