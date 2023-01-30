e67c74b676631da2f2606400fc11fff30164ffbe
ijanggmu <sthaejan00@gmail.com>
2023-01-30 20:44:06 +0545

    :sparkles:(Features)Added Credentials and CommonConstant to store CommonCosntant

---
+++ Bus.Web/Models/Credentials.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bus.Web.Models
+{
+    public class Credentials
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Remember Me")]
+        public bool IsRememberMe { get; set; }
+    }
+}
